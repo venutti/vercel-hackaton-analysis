47935130b4e147fe10fbb4c87af237eed5ec421f
venutti <lucasvenutti23@gmail.com>
2024-07-29 23:00:42 -0300

    Fix main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-import IssuesInfo from "@/components/issues/issues-info";
-import { Button } from "@/components/ui/button";
-import { SparklesIcon } from "lucide-react";
-import { Suspense } from "react";
+import ProjectsResume from "@/components/resume/projects-resume";
+import ProjectsTimeline from "@/components/time/projects-timeline";
+import { getProjects } from "@/lib/issues";
 
-export default function Home() {
+export default async function Home() {
+  const projects = await getProjects();
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 ">
-      <Suspense fallback={<div>Cargando...</div>}>
-        <IssuesInfo />
-      </Suspense>
+    <main className="flex min-h-screen flex-col items-center py-14 px-24 gap-10">
+      <header>
+        <h1 className="text-4xl font-bold">Hackatón Vercel 2024</h1>
+        <p className="text-sm text-center text-muted-foreground">
+          9 de Julio - 6 de Agosto
+        </p>
+      </header>
+      <div className="flex flex-col gap-10 lg:flex-row">
+        <ProjectsResume projects={projects} />
+        <ProjectsTimeline projects={projects} />
+      </div>
     </main>
   );
 }

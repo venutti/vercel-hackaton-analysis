d4210f276e65eec6b47af6bb6f72032ef435c9e2
venutti <lucasvenutti23@gmail.com>
2024-07-31 00:05:57 -0300

    Add project table title

--- src/components/table/projects-table.tsx
+++ src/components/table/projects-table.tsx
@@ -6,5 +6,10 @@ type Props = {
 };
 
 export default function ProjectsTable({ projects }: Props) {
-  return <TableContainer projects={projects} />;
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">Proyectos</h2>
+      <TableContainer projects={projects} />
+    </div>
+  );
 }

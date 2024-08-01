a900c31629d2cf01335f312eee205214f2989651
venutti <lucasvenutti23@gmail.com>
2024-07-31 21:27:17 -0300

    Replace Alert with Accordion in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,10 @@
 import { TriangleAlertIcon } from "lucide-react";
-import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
 
 export default function Header() {
   return (
@@ -9,46 +14,50 @@ export default function Header() {
         9 de Julio - 6 de Agosto
       </p>
 
-      <Alert className="max-w-[85ch] mt-6 p-6">
-        <AlertTitle className="mb-4 flex gap-2 text-yellow-500">
-          <TriangleAlertIcon className="size-4" />
-          ¡Leer con atención!
-        </AlertTitle>
-        <AlertDescription>
-          Esta página está diseñada para extraer y analizar los datos de los
-          proyectos que participan en el Hackatón Vercel 2024. Mediante el uso
-          del Vercel SDK AI se evalúan y analizan TODOS los proyectos,
-          puntuándolos y categorizándolos de manera imparcial.{" "}
-          <a
-            className="text-purple-500 hover:underline"
-            target="_blank"
-            href="https://github.com/venutti/vercel-hackaton-analysis"
-          >
-            Acá podés ver el repo.
-          </a>
-          <br />
-          <br />
-          Las métricas generadas son absolutamente objetivas. No se premiará con
-          un mayor puntaje al autor de esta página. Aunque me encantaría ganar,
-          el repositorio es público. Si querés perder horas y horas tratando de
-          ver si hice trampa podés hacerlo. Suerte con eso ;).
-          <br />
-          <br />
-          <strong>¿Descubriste que hice trampa?</strong> ¡Perfecto! Podés
-          presentar tu queja directamente al{" "}
-          <a
-            className="text-purple-500 hover:underline"
-            target="_blank"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          >
-            Instagram de Midu
-          </a>
-          , botón.
-          <br />
-          <br />
-          ¡Que gane el mejor!
-        </AlertDescription>
-      </Alert>
+      <Accordion className="w-[80ch]" type="single" collapsible>
+        <AccordionItem value="item-1" className="border-b-0">
+          <AccordionTrigger>
+            <div className="flex gap-2 text-yellow-500 text-lg items-center">
+              <TriangleAlertIcon className="size-6" />
+              ¡Leer con atención!
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            Esta página está diseñada para extraer y analizar los datos de los
+            proyectos que participan en el Hackatón Vercel 2024. Mediante el uso
+            del Vercel SDK AI se evalúan y analizan TODOS los proyectos,
+            puntuándolos y categorizándolos de manera imparcial.{" "}
+            <a
+              className="text-purple-500 hover:underline"
+              target="_blank"
+              href="https://github.com/venutti/vercel-hackaton-analysis"
+            >
+              Acá podés ver el repo.
+            </a>
+            <br />
+            <br />
+            Las métricas generadas son absolutamente objetivas. No se premiará
+            con un mayor puntaje al autor de esta página. Aunque me encantaría
+            ganar, el repositorio es público. Si querés perder horas y horas
+            tratando de ver si hice trampa podés hacerlo. Suerte con eso ;).
+            <br />
+            <br />
+            <strong>¿Descubriste que hice trampa?</strong> ¡Perfecto! Podés
+            presentar tu queja directamente al{" "}
+            <a
+              className="text-purple-500 hover:underline"
+              target="_blank"
+              href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            >
+              instagram de Midu
+            </a>
+            , buchón.
+            <br />
+            <br />
+            ¡Que gane el mejor!
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </header>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -69,12 +69,12 @@ const config = {
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.3s ease-out",
+        "accordion-up": "accordion-up 0.3s ease-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
